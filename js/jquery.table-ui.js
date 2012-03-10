cbb8d631ec19ea5bb467f85f3f3913198b348269
hy0kl <hy0kle@gmail.com>
2012-03-10 23:51:00 +0800

    Add jquery plugin, tableUI.

---
+++ js/jquery.table-ui.js
@@ -0,0 +1,27 @@
+(function($){
+    $.fn.tableUI = function(settings) {
+        var defaults = {
+            evenClass:   'evenRow',
+            oddClass:    'oddRow',
+            activeClass: 'activeRow'
+        };
+
+        if (settings)
+        {
+            $.extend(defaults, settings);
+        }
+        
+        this.each(function(){
+            var table = $(this);
+            table.find('tr:even').addClass(defaults.evenClass);
+            table.find('tr:odd').addClass(defaults.oddClass);
+
+            table.find('tr').bind('mouseover', function(){
+                $(this).addClass(defaults.activeClass);    
+            });
+            table.find('tr').bind('mouseout', function(){
+                $(this).removeClass(defaults.activeClass);    
+            });
+        });
+    }; 
+})(jQuery);
