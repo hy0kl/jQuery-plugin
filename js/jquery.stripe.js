ecc95988370eb587499cb2326b3d7cef9c96c237
hy0kl <hy0kle@gmail.com>
2012-03-11 18:39:04 +0800

    Add new jQuery plugin: stripe.

---
+++ js/jquery.stripe.js
@@ -0,0 +1,36 @@
+(function($){
+    $.fn.stripe = function(settings) {
+        var defaults = {
+            evenClass:   'evenNode',
+            oddClass:    'oddNode',
+            activeClass: 'activeNode'
+        };
+
+        if (settings)
+        {
+            $.extend(defaults, settings);
+        }
+
+        var i = 1;
+        this.each(function(){
+            var node = $(this);
+
+            if (1 == i % 2)
+            {
+                node.addClass(defaults.evenClass);
+            else
+            {
+                node.addClass(defaults.oddClass);
+            }
+
+            node.bind('mouseover', function(){
+                $(this).addClass(defaults.activeClass);
+            });
+            node.bind('mouseout', function(){
+                $(this).removeClass(defaults.activeClass);
+            });
+
+            i++;
+        });
+    };
+})(jQuery);
